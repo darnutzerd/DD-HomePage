17f0a96fb8ee92c63b20a83ce99a5ad543d876e3
David Darnutzer <50007019@fvtc.edu>
2026-09-01 17:44:55 -0500

    Exercise 1 and 2

---
+++ site.js
@@ -0,0 +1,6 @@
+const mainContentSection = document.querySelector('h2');
+mainContentSection.style.color = 'red';
+
+const link = document.querySelector('footer a');
+link.style.textDecoration = 'underline';
+
